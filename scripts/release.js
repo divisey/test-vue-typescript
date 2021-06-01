180d7d7cbd5a51b47fa507f41ef5850d710a9191
zhangqingwu <18656007202@163.com>
2021-06-01 14:52:07 +0800

    fix: fix scripts

--- scripts/release.js
+++ scripts/release.js
@@ -4,7 +4,7 @@ const execSync = require("./execSync");
 
 console.log("$ test ghrelease");
 
-const GH_RELEASE_FILES = "[{ 'test-typescript-vue-dist.zip': './dist' }]";
+const GH_RELEASE_FILES = "[{'test-typescript-vue-dist.zip':'./dist'}]";
 const files = GH_RELEASE_FILES ? `--assets ${GH_RELEASE_FILES}` : "";
 
 console.log(`$ yarn foxone-exec-ghrelease --draft ${files} --yes`);
